d939e51a97b844c8fa2d82365c48540b70439f63
Kirill Kozionov <kirill@kozionov.com>
2015-09-24 00:19:21 -0400

    [*] ))))1wqwqw

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var server = app.listen(process.env.port, function () {
   console.info('Express server started');
 });
 
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+  'resource': '/socket.io',
+  'path': '/socket.io',
+  'match origin protocol': true
+});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
