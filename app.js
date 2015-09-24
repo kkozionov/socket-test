51ab5558147e96c6e33a15055620b0d679c83cfb
Kirill Kozionov <kirill@kozionov.com>
2015-09-23 23:18:42 -0400

    [*] some test 5

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ var server = app.listen(process.env.port || 3000, function () {
   console.info('Express server started');
 });
 
-var io = require('socket.io').listen(server, {
-    'transports': ['websocket', 'polling'],
-    'path': '/ws/socket.io'
-});
+var io = require('socket.io').listen(server);
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
